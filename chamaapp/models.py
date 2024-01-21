445a89020d6d28e7137eb63bb81c149dd6c79e55
RUBANIKAHIU <georgekahiu@outlook.com>
2024-01-22 00:23:24 +0300

    linked sign up page with backend,madelist of members page and linked it

--- chamaapp/models.py
+++ chamaapp/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Profile (models.Model):
     GENDER_CHOICES = (
         ('M' ,'Male'),
         ('F' ,'Female')
     )
     user=models.OneToOneField(User, default=1, on_delete=models.CASCADE)
-    profile_user_id=models.AutoField(primary_key=True, default=1 ,blank=False, null=False)
+    profile_user_id=models.AutoField(primary_key=True,blank=False, null=False)
     first_name = models.CharField(max_length=50)
     last_name =models.CharField(max_length=50)
     phone_number= models.IntegerField(unique=True,blank=False)
@@ -16,5 +17,15 @@ class Profile (models.Model):
     gender=models.CharField(max_length= 8, choices=GENDER_CHOICES)
     date_of_birth=models.DateField()
 
+
     def __str__(self):
         return self.first_name
+
+class Members(models.Model):
+    profile = models.OneToOneField(Profile, default=1, on_delete=models.CASCADE)
+    phone_number = models.IntegerField(unique=True, blank=False)
+    email = models.EmailField(unique=True, blank=False, max_length=100)
+
+    def __str__(self):
+        return str(self.phone_number)
+
--- chamaapp/views.py
+++ chamaapp/views.py
@@ -1,5 +1,7 @@
 #Rendering html pages
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.db import IntegrityError # occurs when there is an attempt to insert or update data that violates constraints such as unique constraints, foreign key constraints, or other database-specific constraints.
 
 #rest_framework codes
 # Importing Serializer
@@ -12,8 +14,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
 
+
+from django.contrib.auth.models import User
+
 #importing my models
-from .models import Profile 
+from .models import Profile,Members
+
 
 
 class DummySerializer(serializers.Serializer):
@@ -28,13 +34,100 @@ class chamaviewset(viewsets.ModelViewSet):
     @action(detail=False, methods= ['get','post'])
     def signup(self,request):
         if request.method == 'POST':
-            print('am here')
+            first_name = request.POST.get('fname')
+            last_name = request.POST.get('lname')
+            phone_number = request.POST.get('phone')
+            email= request.POST.get('email')
+            gender= request.POST.get('gender')
+            date_of_birth= request.POST.get('dob')
+            password= request.POST.get('password')
+
+            
+             # Check if phone number already exists
+            if User.objects.filter(username=phone_number).exists():
+                phone_numberError = "The  phone number already exits"
+                return render(request, 'signup.html', {'phone_numberError': phone_numberError, 'emailError': ''})
+
+            # Check if email already exists
+            if User.objects.filter(email=email).exists():
+                emailError = "The email already exits"
+                return render(request, 'signup.html', {'emailError': emailError, 'phone_numberError': ''})
+            
+            try:
+                    # Create user in chamaapp
+                    user = User.objects.create_user(
+                        username=phone_number,
+                        email=email,
+                        password=password
+                    )
+
+                    
+                    # Create user profile in chamaapp
+                    profile = Profile.objects.create (
+                        user=user,
+                        first_name = first_name,
+                        last_name =last_name,
+                        phone_number= phone_number,
+                        email=email,
+                        gender=gender,
+                        date_of_birth=date_of_birth
+                    )
+
+
+
+
+                    # Redirect to success page or perform other actions
+                    profile.save() 
+                    return redirect('http://127.0.0.1:8000/user_login/')
+            
+            except IntegrityError as e:
+                 print(f"Error {e}")
+            
         return render(request, 'signup.html')
-    
+      
 #Render login.html
     @action(detail=False, methods= ['get','post'])
-    def login(self,request):
+    def user_login(self,request):
+
+        if request.method == 'POST':
+             
+             phone_number = request.POST.get('phone')
+             password = request.POST.get('password')
+             user = User.objects.filter(username=phone_number).first()
+
+
+             if user:
+                  print(user)
+                  if user.check_password(password):
+                       return redirect ('http://127.0.0.1:8000/home/')
+
+                  else:
+                       print('unsuccesful login')
+                       
+             else:
+                print('User not found')
+             
+                  
         return render(request, 'login.html')
+    
+#Render homepage
+    @action(detail=False, methods= ['get','post'])
+    def home(self,request):
+        return render(request, 'homepage.html')
+    
+#render totalfunds
+    @action(detail=False, methods= ['get','post'])
+    def totalfunds(self,request):
+        return render(request, 'totalfunds.html')
+
+#render list of members
+    @action(detail=False, methods= ['get','post'])
+    def members_list(self,request):
+        members = Members.objects.select_related('profile').all()
+        return render(request, 'listofmembers.html',  {'members': members})
+
+        
+
 
     
 
